809b78ee202aafa58648882663cd6ea6d8b3b218
H3rzius <h3rzius@gmail.com>
2022-11-01 18:06:55 +0100

    1.1.8 part 2 completing hack cmd

--- src/main/java/me/h3rzius/h3rcustomcommands/commands/HackCommand.java
+++ src/main/java/me/h3rzius/h3rcustomcommands/commands/HackCommand.java
@@ -11,21 +11,34 @@ import org.bukkit.entity.Player;
 
 public class HackCommand implements CommandExecutor {
     FileConfiguration config;
+
+    /**
+     *
+     * @param sender Source of the command
+     * @param command Command which was executed
+     * @param s Alias of the command which was used
+     * @param args Passed command arguments
+     * @return
+     */
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         config = Bukkit.getServer().getPluginManager().getPlugin("H3rCustomCommands").getConfig();
+        /** Check if the cmd is enabled in config.yml. Else the cmd disabled until enabling. */
         if (config.getBoolean("enable-hack-cmd")) {
             String timeBan = config.getString("time-ban");
             String timeBanIP = config.getString("time-banip");
             String reason = config.getString("reason-ban");
             String reasonIP = config.getString("reason-banip");
-            if (args.length == 0) {
-                sender.sendMessage(ChatColor.RED + config.getString("hack-cmd-help"));
-            } else {
-                Player p = Bukkit.getPlayer(args[0]);
-                String ip = p.getAddress().getHostString();
-                Bukkit.dispatchCommand(sender, "ban -s " + p + " " + timeBan + " " + reason);
-                Bukkit.dispatchCommand(sender, "ban-ip -s " + ip + " " + timeBanIP + " " + reasonIP);
+            /** Check if player or console has permissions to execute this cmd */
+            if (sender.hasPermission("h3rcustomcommands.hack")) {
+                if (args.length == 0) {
+                    sender.sendMessage(ChatColor.RED + config.getString("hack-cmd-help"));
+                } else {
+                    Player p = Bukkit.getPlayer(args[0]);
+                    String ip = p.getAddress().getHostString();
+                    Bukkit.dispatchCommand(sender, "ban -s " + p + " " + timeBan + " " + reason);
+                    Bukkit.dispatchCommand(sender, "ban-ip -s " + ip + " " + timeBanIP + " " + reasonIP);
+                }
             }
             return true;
         } else {
